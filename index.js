1d0f77097b70d0bbd2fb6e68127f65665ca69122
amaryahwolf <115281632+amaryahwolf@users.noreply.github.com>
2022-12-21 09:23:50 -0800

    add user input questions

--- index.js
+++ index.js
@@ -4,10 +4,73 @@ const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
 const questions = [
-    {type: 'input',
-        message: "Enter the title of your project: ",
-        name: "title",
-    }
+    {
+        type: 'input',
+        message: 'Enter the title of your project:',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Provide a short description explaining the whay, why, and how of your project. Use the following questions as a guide:\nWhat was your motivation?\nWhy did you build this project?\nWhat problem does it solve?\nWhat did you learn?',
+        name: 'description'
+    },
+    {
+        type: 'checkbox',
+        message: 'What do you want included in your table of contents?',
+        choices: [
+          'Installation',
+          'Usage',
+          'Credits',
+          'License',
+        ],
+        name: 'tableOfContents',
+    },
+    {
+        type: 'input',
+        message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.',
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: 'Enter instructions and examples for use:',
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: 'Enter the file path for a screenshot of your project:',
+        name: 'screenshot',
+    },
+    {
+        type: 'input',
+        message: 'List your collaborators, any third-party assets, and/or tutorials followed, if any:',
+        name: 'credits',
+    },
+    {
+        type: 'list',
+        message: 'Which license did you use?',
+        choices: ['MIT', 'GPL', 'Apache', 'None'],
+        name: 'license'
+    },
+    {
+        type: 'input',
+        message: 'Enter your contribution guidelines:',
+        name: 'contributions',
+    },
+    { 
+        type: 'input',
+        message: 'Enter your test guidelines:',
+        name: 'tests',
+    },
+    {
+        type: 'input',
+        message: 'Enter your github username:',
+        name: 'github',
+    },
+    {
+        type: 'input',
+        message: 'Enter your email:',
+        name: 'email',
+    },
 ];
 
 // TODO: Create a function to write README file
@@ -25,7 +88,6 @@ function init() {
     .prompt (questions)
     .then((response)=> {
         writeToFile("README.md", generateMarkdown(response))
-    // generateMarkdown
         console.log(response)
     }
     )
