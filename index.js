efe40ab7a0f9d5d478aace0c737af4a97bdc9084
amaryahwolf <115281632+amaryahwolf@users.noreply.github.com>
2022-12-20 18:56:30 -0800

    add function starters

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {type: 'input',
+        message: "Enter the title of your project: ",
+        name: "title",
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if(err) throw err 
+        console.log("Input received!")
+    }
+    )
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+    .prompt (questions)
+    .then((response)=> {
+        writeToFile("README.md", generateMarkdown(response))
+    // generateMarkdown
+        console.log(response)
+    }
+    )
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,40 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== "none") {
+    return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+  } else {
+    return ""
+  }
+  
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== "none") {
+    return `- [License](#license)`
+  } else {
+    return ``
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== "none") {
+    return `## License
+    My license is ${license}`
+  } else {
+    return `` 
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+ 
+  ${renderLicenseBadge(data.license)}
 
 `;
 }
